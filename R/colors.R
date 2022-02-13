2b5edfdf443c2440677bf90a069f8ed4c704a505
Andrei Martins <andrei.silva@gupy.com.br>
2022-02-13 12:40:34 -0300

    Improve head to head chart appearance

---
+++ R/colors.R
@@ -0,0 +1,27 @@
+scuderia_colors <- function(){
+  colors <- c(
+    "#91251E",
+    "#2b4562",
+    "#198ffb",
+    "#387063",
+    "#dd3d33",
+    "#f5f3ed",
+    "#ef9734",
+    "#6ed4c0",
+    "#3865ef",
+    "#206afc"
+  )
+  names(colors) <- c(
+      "Alfa Romeo Racing Ferrari",
+      "AlphaTauri Honda",
+      "Alpine Renault",
+      "Aston Martin Mercedes",
+      "Ferrari",
+      "Haas Ferrari",
+      "McLaren Mercedes",
+      "Mercedes",
+      "Red Bull Racing Honda",
+      "Williams Mercedes"
+    )
+  colors
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Formula 1
 
 I love Formula 1, so I decided to explore some data and maybe build a couple of
-models. This project is intended to hold analysis, eventually some predictive
+models. In this project, I'll create some analysis, eventually some predictive
 models and fancy charts like the one below:
 
 ![Head to Head Drivers Comparison Chart](./sample_chart.png)
